6a3663088803a17060461bf301273a564b152a43
macwhirr <mac_you@163.com>
2019-03-13 12:02:08 +0800

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,15 @@
+(function(){
+var width = window.innerWidth,
+  height = parseInt(parseInt(width* (9/16) )),
+  height = height > window.innerHeight ? window.innerHeight : height;
+
+window.tvpSetting = {
+  width: width,
+  height: height,
+  vid: 'l05309p8lxw',
+  vid2: 'o0171fmfwmf',
+  vid_limit: 'z0016v1ocio',
+  vid_short: 'f162169s71x',
+  lid: '123002500'
+};
+})();
